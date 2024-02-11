7a328d1b4f7bee54a628b5c94903336d552f4236
Madison Dail <madisonrosed@gmail.com>
2024-02-11 13:12:23 -0500

    update code

--- player.py
+++ player.py
@@ -18,3 +18,25 @@ class Player(Entity):                       #inherit from entity
 
     def update(self):                       #check for key press for every update
         self.movement()
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = BLOCK_LAYER
+        self.groups = self.game.all_sprites, self.game.blocks
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        #block is square 32x32 pixels
+        self.x = x * TILE_SIZE
+        self.y = y * TILE_SIZE
+        self.width = TILE_SIZE 
+        self.height = TILE_SIZE
+
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        self.rect.width = self.width
+        self.rect.height = self.height
+        
