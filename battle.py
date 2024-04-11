834e210b12467051c83279df14289f132e8da9e4
Madison Dail <madisonrosed@gmail.com>
2024-04-11 15:36:54 -0400

    battle screen looks better, not incorporated with game yet

--- battle.py
+++ battle.py
@@ -1,24 +1,18 @@
 import pygame
 
-# Initialize Pygame
 pygame.init()
-
-# Set up the display
 screen = pygame.display.set_mode((640, 480))
 
 # Load the Pokémon images
 player_pokemon_image = pygame.image.load('images/pokemon/oshawott.png').convert_alpha()
-opponent_pokemon_image = pygame.image.load('images/pokemon/pika_battle.webp').convert_alpha()
+opponent_pokemon_image = pygame.image.load('images/pokemon/pikachu.png').convert_alpha()
 
 font = pygame.font.Font('Adobe Garamond Pro Regular.ttf', 35)
+fsu_gold = (206, 184, 136)
 
-#  text surfaces
-player_poke = font.render("Pokemon", True, (0, 0, 0))
-poke_name = font.render("Pokemon 2", True, (0, 0, 0))
-run = font.render("Run", True, (0, 0, 0))
-ball = font.render("Ball", True, (0, 0, 0))
-fight = font.render("Fight", True, (0, 0, 0))
-pokemon = font.render("Pokémon", True, (0, 0, 0))
+def display_text(txt, font, color, x, y):
+        text = font.render(txt, True, color)
+        screen.blit(text, (x, y))
 
 # Main game loop
 running = True
@@ -28,19 +22,21 @@ while running:
             running = False
         #need to add player clicks and stuff 
  
-    screen.fill((255, 255, 255))  
+    screen.fill((120, 47, 64))  
 
     # Draw Pokémon images
-    screen.blit(player_pokemon_image, (50, 100))  
-    screen.blit(opponent_pokemon_image, (400, 50))  
+    screen.blit(player_pokemon_image, (100, 100))  
+    screen.blit(opponent_pokemon_image, (350, 100))  
 
     # Draw text surfaces
-    screen.blit(player_poke, (50, 50))  
-    screen.blit(poke_name, (400, 10)) 
-    screen.blit(run, (50, 350))  
-    screen.blit(ball, (150, 350))  
-    screen.blit(fight, (250, 350))  
-    screen.blit(pokemon, (350, 350))  
+    display_text("Oshawott", font, fsu_gold, 100, 50)
+    display_text("Pikachu", font, fsu_gold, 400, 50)
+    display_text("HP 87/87", font, fsu_gold, 100, 100)
+    display_text("HP 87/87", font, fsu_gold, 400, 100)
+    display_text("Run", font, fsu_gold, 150, 300)
+    display_text("Ball", font, fsu_gold, 400 , 300)
+    display_text("Fight", font, fsu_gold, 150, 350)
+    display_text("Pokémon", font, fsu_gold, 400, 350) 
     # Update display
     pygame.display.flip()
 
